65ddd178a74a29c3598655821aa448c8c8c8da9f
Shane D. Tyler <soniccinema@yahoo.com>
2021-09-04 14:20:33 -0700

    somewhat finished

---
+++ db/index.js
@@ -0,0 +1,36 @@
+const { Client } = require('pg');
+const client = new Client('postgres://localhost:5432/juicebox-dev');
+
+module.exports = {
+    client,
+}
+
+async function createUser({ username, password }) {
+    try {
+        const result = await client.query(`
+        INSERT INTO users(username, password)
+      VALUES ($1, $2);
+      ON CONFLICT (username) DO NOTHING
+      RETURNING *;
+
+    `, [username, password]);
+
+        return result
+    } catch (error) {
+        throw error;
+    }
+}
+async function getAllUsers() {
+    const { rows } = await client.query(
+        `SELECT id, username
+        FROM users;
+    `);
+    return rows;
+    
+}
+
+module.exports = {
+    client,
+    getAllUsers,
+    createUser
+}
