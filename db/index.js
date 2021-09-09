8f0f51c10d7b4c5db087baaf3f6a2f65940b47cf
Shane D. Tyler <soniccinema@yahoo.com>
2021-09-08 21:09:22 -0700

    pushing Move Into Node

--- db/index.js
+++ db/index.js
@@ -1,36 +1,47 @@
-const { Client } = require('pg');
+// inside db/index.js
+const { Client } = require('pg'); // imports the pg module
+
+// supply the db name and location of the database
 const client = new Client('postgres://localhost:5432/juicebox-dev');
 
 module.exports = {
     client,
 }
 
-async function createUser({ username, password }) {
-    try {
-        const result = await client.query(`
-        INSERT INTO users(username, password)
-      VALUES ($1, $2);
-      ON CONFLICT (username) DO NOTHING
-      RETURNING *;
 
-    `, [username, password]);
+// const { Client } = require('pg');
+// const client = new Client('postgres://localhost:5432/juicebox-dev');
 
-        return result
-    } catch (error) {
-        throw error;
-    }
-}
-async function getAllUsers() {
-    const { rows } = await client.query(
-        `SELECT id, username
-        FROM users;
-    `);
-    return rows;
+// module.exports = {
+//     client,
+// }
+
+// async function createUser({ username, password }) {
+//     try {
+//         const result = await client.query(`
+//         INSERT INTO users(username, password)
+//       VALUES ($1, $2);
+//       ON CONFLICT (username) DO NOTHING
+//       RETURNING *;
+
+//     `, [username, password]);
+
+//         return result
+//     } catch (error) {
+//         throw error;
+//     }
+// }
+// async function getAllUsers() {
+//     const { rows } = await client.query(
+//         `SELECT id, username
+//         FROM users;
+//     `);
+//     return rows;
     
-}
+// }
 
-module.exports = {
-    client,
-    getAllUsers,
-    createUser
-}
+// module.exports = {
+//     client,
+//     getAllUsers,
+//     createUser
+// }
