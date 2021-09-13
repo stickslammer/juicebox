8595be3cf6638a7a9b52dde64d6844c76c774108
Shane D. Tyler <soniccinema@yahoo.com>
2021-09-12 18:56:40 -0700

    Do over, Updated Users Table

--- db/seed.js
+++ db/seed.js
@@ -42,10 +42,9 @@ async function createInitialUsers() {
     try {
         console.log("Starting to create users...");
 
-        await createUser({ username: 'albert', password: 'bertie99' });
-        await createUser({ username: 'sandra', password: '2sandy4me' });
-        await createUser({ username: 'glamgal', password: 'soglam' });
-
+        await createUser({ username: 'albert', password: 'bertie99', name: 'Al Bert', location: 'Sidney, Australia', active: true });
+        await createUser({ username: 'sandra', password: '2sandy4me', name: 'Just Sandra', location: "Ain't Telling", active: true });
+        await createUser({ username: 'glamgal', password: 'soglam', name: 'Joshua', location: 'Upper East Side', active: true });
         console.log("Finished creating users!");
     } catch (error) {
         console.error("Error creating users!");
