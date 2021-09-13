c54de34e0efb55d8a57591d3ce4688b490433741
Shane D. Tyler <soniccinema@yahoo.com>
2021-09-12 20:27:31 -0700

    Final Push, again

--- db/seed.js
+++ db/seed.js
@@ -117,6 +117,7 @@ async function rebuildDB() {
         await createTables();
         await createInitialUsers();
         await createInitialPosts();
+        await createInitialTags();
     } catch (error) {
         console.log("Error during rebuildDB");
         throw error;
