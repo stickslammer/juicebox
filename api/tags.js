9d06c07639b20765711dc5a4db91f584f5c0996f
Shane D. Tyler <soniccinema@yahoo.com>
2021-09-18 15:09:36 -0700

    about to write DELETE...

--- api/tags.js
+++ api/tags.js
@@ -7,11 +7,21 @@ tagsRouter.use((req, res, next) => {
 
     next();
 });
-tagsRouter.get('/', async (req, res) => {
-    const tags = await getAllTags();
-    res.send({
-        tags
-    });
+
+tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+    // read the tagname from the params
+    try {
+        // use our method to get posts by tag name from the db
+        const tags = await getAllTags();
+        // send out an object to the client { posts: // the posts }
+        res.send({
+            tags
+        });
+    } catch ({ name, message }) {
+        console.log(error);
+        next(error);
+    }
+        
 });
 
 module.exports = tagsRouter;
