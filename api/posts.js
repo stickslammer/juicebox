e38f45fa9775548f377d729ebe731de873b6a70c
Shane D. Tyler <soniccinema@yahoo.com>
2021-09-18 14:57:46 -0700

    about to write GET...

--- api/posts.js
+++ api/posts.js
@@ -41,4 +41,39 @@ postsRouter.post('/', requireUser, async (req, res, next) => {
     }
 });
 
+postsRouter.patch('/:postId', requireUser, async (req, res, next) => {
+    const { postId } = req.params;
+    const { title, content, tags } = req.body;
+
+    const updateFields = {};
+
+    if (tags && tags.length > 0) {
+        updateFields.tags = tags.trim().split(/\s+/);
+    }
+
+    if (title) {
+        updateFields.title = title;
+    }
+
+    if (content) {
+        updateFields.content = content;
+    }
+
+    try {
+        const originalPost = await getPostById(postId);
+
+        if (originalPost.author.id === req.user.id) {
+            const updatedPost = await updatePost(postId, updateFields);
+            res.send({ post: updatedPost })
+        } else {
+            next({
+                name: 'UnauthorizedUserError',
+                message: 'You cannot update a post that is not yours'
+            })
+        }
+    } catch ({ name, message }) {
+        next({ name, message });
+    }
+});
+
 module.exports = postsRouter;
