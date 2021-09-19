bd175f0275bdb1d2479123f9a67bdbfec2171675
Shane D. Tyler <soniccinema@yahoo.com>
2021-09-18 22:36:32 -0700

    got tagsRouter.get to work

--- api/tags.js
+++ api/tags.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const tagsRouter = express.Router();
-const { getAllTags} = require('../db');
+const { getAllTags, getPostsByTagName} = require('../db');
 
 tagsRouter.use((req, res, next) => {
     console.log("A request is being made to /tags");
@@ -10,12 +10,13 @@ tagsRouter.use((req, res, next) => {
 
 tagsRouter.get('/:tagName/posts', async (req, res, next) => {
     // read the tagname from the params
+    const { tagName } = req.params;
     try {
         // use our method to get posts by tag name from the db
-        const tags = await getAllTags();
+        const postsByTagName = await getPostsByTagName( tagName );
         // send out an object to the client { posts: // the posts }
         res.send({
-            tags
+            posts: postsByTagName
         });
     } catch ({ name, message }) {
         console.log(error);
