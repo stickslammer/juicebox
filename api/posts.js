89108f096280043df035a9511034bdf9e0ec67ba
Shane D. Tyler <soniccinema@yahoo.com>
2021-09-18 21:35:09 -0700

    about to write DELETE...again

--- api/posts.js
+++ api/posts.js
@@ -10,10 +10,19 @@ postsRouter.use((req, res, next) => {
     next();
 });
 postsRouter.get('/', async (req, res) => {
-    const post = await getAllPosts();
-    res.send({
-        post
-    });
+    try {
+        const allPosts = await getAllPosts();
+
+        const posts = allPosts.filter(post => {
+            return post.active || (req.user && post.author.id === req.user.id);
+        });
+
+        res.send({
+            posts
+        });
+    } catch ({ name, message }) {
+        next({ name, message });
+    }
 });
 postsRouter.post('/', requireUser, async (req, res, next) => {
     const { title, content, tags = "" } = req.body;
@@ -73,6 +82,30 @@ postsRouter.patch('/:postId', requireUser, async (req, res, next) => {
         }
     } catch ({ name, message }) {
         next({ name, message });
+    }
+});
+
+postsRouter.delete('/:postId', requireUser, async (req, res, next) => {
+    try {
+        const post = await getPostById(req.params.postId);
+
+        if (post && post.author.id === req.user.id) {
+            const updatedPost = await updatePost(post.id, { active: false });
+
+            res.send({ post: updatedPost });
+        } else {
+            // if there was a post, throw UnauthorizedUserError, otherwise throw PostNotFoundError
+            next(post ? {
+                name: "UnauthorizedUserError",
+                message: "You cannot delete a post which is not yours"
+            } : {
+                name: "PostNotFoundError",
+                message: "That post does not exist"
+            });
+        }
+
+    } catch ({ name, message }) {
+        next({ name, message })
     }
 });
 
--- db/index.js
+++ db/index.js
@@ -195,12 +195,17 @@ async function getAllPosts() {
 
 async function getPostById(postId) {
     try {
-        const { rows: [post] } = await client.query(`
+        const { rows: [ post ] } = await client.query(`
       SELECT *
       FROM posts
       WHERE id=$1;
     `, [postId]);
-
+        if (!post) {
+            throw {
+                name: "PostNotFoundError",
+                message: "Could not find a post with that postId"
+            };
+        }
         const { rows: tags } = await client.query(`
       SELECT tags.*
       FROM tags
